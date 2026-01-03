871a2dfceb7380b3baadb6cc067d1feb16c3fda8
Eman Fatima <emanfatima.37303@gmail.com>
2026-01-03 10:03:36 -0800

    Update print_alternates.py

--- print_alternates.py
+++ print_alternates.py
@@ -3,7 +3,7 @@ class Solution:
         # Code Here
         #slicing
         result = []       # to not change original array
-        for i in range(0, len(arr), 2): # step by 2
+        for i in range(0, len(arr) , 2): # step by 2
             result.append(arr[i]) 
         return result# to add element in array
        
