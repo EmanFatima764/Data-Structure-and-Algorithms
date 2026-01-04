bfd6365db45b180d501331d03773f624e07c57ff
Eman Fatima <emanfatima.37303@gmail.com>
2026-01-04 08:05:43 -0800

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -7,17 +7,7 @@ class Solution:
         if arr[n-1]<arr[n-2]:
             return False
         return self.isSorted(arr[:n-1])
-      # recursion
-      class Solution:
-    def isSorted(self, arr) -> bool:
-        return self.check(arr,0)
-    def check(self,arr,i):
-        if i == len(arr)-1:
-            return True
-        if arr[i]>arr[i+1]:
-            return False
-        return self.check(arr,i+1)
-        # iterative approach
+      
     
         
       
