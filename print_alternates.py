951e0999809bfcba55628066f50577a3591fbd81
Eman Fatima <emanfatima.37303@gmail.com>
2026-01-05 08:39:27 -0800

    Update print_alternates.py

--- print_alternates.py
+++ print_alternates.py
@@ -5,5 +5,5 @@ class Solution:
         result = []       # to not change original array
         for i in range(0, len(arr) , 2): # step by 2
             result.append(arr[i]) 
-        return result# to add element in array
+        return result    # to add element in array
        
