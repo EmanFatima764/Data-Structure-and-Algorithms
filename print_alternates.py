fe8cb35cc2a6f48f4df5258851f3728707330869
Eman Fatima <emanfatima.37303@gmail.com>
2026-01-09 08:58:54 -0800

    Update print_alternates.py

--- print_alternates.py
+++ print_alternates.py
@@ -2,7 +2,7 @@ class Solution:
     def getAlternates(self, arr):
         # Code Here
         #slicing
-        result = []       # to not change original array
+        result = []           # to not change original array
         for i in range(0, len(arr) , 2): # step by 2
             result.append(arr[i]) 
         return result    # to add element in array
