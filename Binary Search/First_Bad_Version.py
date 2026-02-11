94640683afadd5fc1b834e3b92ea8d5e51da509a
EmanFatima764 <emanfatima.37303@gmail.com>
2026-02-11 08:54:25 -0800

    Add " First_Bad_Version.py" in File

---
+++ Binary Search/First_Bad_Version.py
@@ -0,0 +1,22 @@
+# The isBadVersion API is aLeftready defined for you.
+# def isBadVersion(version: int) -> bool:
+
+def isBadVersion(version):
+    return version>=4
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+
+        left=1
+        right=n
+        while left<right:
+            mid=left+(right-left)//2
+            if isBadVersion(mid):  # first bad version is at mid or before
+                right=mid
+            else:
+                left=mid+1      # first bad version is after mid
+        return left
+if __name__ =="__main__":
+    sol=Solution()
+    n=5
+    print("First Bad Version:",sol.firstBadVersion(n))
+
