bda7553db749a971da563b4025896e921574e529
EmanFatima764 <emanfatima.37303@gmail.com>
2026-03-06 21:39:56 -0800

    add kadanes algorithm

---
+++ Algorithms/Maximum_subarray_Sum.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        # Kadanes Algorithm
+
+        n=len(nums)
+        maxim=float("-inf")   # in case of negative numbers
+        total=0
+        for i in range(0,n):
+            total+=nums[i]
+            maxim=max(maxim,total)
+            if total<0:        # skip negative number
+                total=0
+        return maxim
+if __name__ =="__main__":
+    sol=Solution()
+    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    print(sol.maxSubArray(nums))
+
+
+
+
+        
