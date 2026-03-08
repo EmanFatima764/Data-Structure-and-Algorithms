a205b8164a4f1b5ad09cac588c883e0c56117d3c
EmanFatima764 <emanfatima.37303@gmail.com>
2026-03-08 08:39:26 -0700

    change some pointers

--- maximum average subarray.py
+++ maximum average subarray.py
@@ -5,7 +5,7 @@ class Solution:
         window_sum=sum(nums[:k])   # sum of first subarray
         max_sum=window_sum
         for i in range(k,len(nums)):   # loop from 4 to 6
-            window_sum =window_sum+nums[i] - nums[k-i] 
+            window_sum =window_sum+nums[i] - nums[i-k] 
             max_sum=max(max_sum,window_sum)
         return max_sum/k           # avg max_sum
 
